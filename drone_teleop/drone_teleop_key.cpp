41b47c37f1529bfc62f9e01b48050d85ca1d25e9
xXmeltdownXx <trent_lowery@yahoo.com>
2022-10-15 17:38:26 -0500

    arm code removed
    cleaned arm code from .cpp file

--- drone_teleop/drone_teleop_key.cpp
+++ drone_teleop/drone_teleop_key.cpp
@@ -67,17 +67,14 @@ private:
 
   void jointStateCallback(const sensor_msgs::JointState::ConstPtr& joint_state);
   void moveBase(ros::Publisher &publisher, const double linear_x, const double angular_z);
-  void moveArm(ros::Publisher &publisher, const std::string &name, const double position, const double current_pos);
-  void moveHand(ros::Publisher &publisher, const double position, const double current_pos);
-  void stopJoints(ros::Publisher &publisher, const int duration_sec);
 
+  
   static int calcTrajectoryDuration(const double val, const double current_val);
 
   void showHelp();
 
   // Current positions that is updated by JointState
-  double joint1_pos1_, joint2_pos1_, joint3_pos1_, joint4_pos1_, grip_joint_pos1_;
-  double joint1_pos2_, joint2_pos2_, joint3_pos2_, joint4_pos2_;
+  
 //  clock_t joint_state_time1_, joint_state_time2_;
 };
 
@@ -162,88 +159,7 @@ void SIGVerseTb3OpenManipulatorGraspingTeleopKey::moveBase(ros::Publisher &publi
 }
 
 
-void SIGVerseTb3OpenManipulatorGraspingTeleopKey::moveArm(ros::Publisher &publisher, const std::string &name, const double position, const double current_pos)
-{
-  std::vector<std::string> names;
-  names.push_back(name);
 
-  std::vector<double> positions;
-  positions.push_back(position);
-
-  ros::Duration duration;
-  duration.sec = calcTrajectoryDuration(position, current_pos);
-
-  trajectory_msgs::JointTrajectory joint_trajectory;
-
-  trajectory_msgs::JointTrajectoryPoint arm_joint_point;
-
-  joint_trajectory.points.push_back(arm_joint_point);
-
-  joint_trajectory.joint_names = names;
-  joint_trajectory.points[0].positions = positions;
-  joint_trajectory.points[0].time_from_start = duration;
-
-  publisher.publish(joint_trajectory);
-}
-
-void SIGVerseTb3OpenManipulatorGraspingTeleopKey::moveHand(ros::Publisher &publisher, const double position, const double current_pos)
-{
-  std::vector<std::string> joint_names {GRIP_JOINT_NAME, GRIP_JOINT_SUB_NAME};
-
-  std::vector<double> positions;
-
-  positions.push_back(position); // for grip_joint
-  positions.push_back(position); // for grip_joint_sub
-
-  ros::Duration duration;
-  duration.sec = calcTrajectoryDuration(position, current_pos);
-
-  trajectory_msgs::JointTrajectoryPoint point;
-  point.positions = positions;
-  point.time_from_start = duration;
-
-  trajectory_msgs::JointTrajectory joint_trajectory;
-  joint_trajectory.joint_names = joint_names;
-  joint_trajectory.points.push_back(point);
-
-  publisher.publish(joint_trajectory);
-}
-
-
-void SIGVerseTb3OpenManipulatorGraspingTeleopKey::stopJoints(ros::Publisher &publisher, const int duration_sec)
-{
-//  std::vector<std::string> names {JOINT1_NAME, JOINT2_NAME, JOINT3_NAME, JOINT4_NAME, GRIP_JOINT_NAME, GRIP_JOINT_SUB_NAME};
-  std::vector<std::string> names {JOINT1_NAME, JOINT2_NAME, JOINT3_NAME, JOINT4_NAME};
-
-//  double coef = (double)(clock()-joint_state_time2_)/(joint_state_time1_-joint_state_time2_);
-
-  std::vector<double> positions;
-//  positions.push_back(coef*(joint1_pos1_-joint1_pos2_)+joint1_pos2_);
-//  positions.push_back(coef*(joint2_pos1_-joint2_pos2_)+joint2_pos2_);
-//  positions.push_back(coef*(joint3_pos1_-joint3_pos2_)+joint3_pos2_);
-//  positions.push_back(coef*(joint4_pos1_-joint4_pos2_)+joint4_pos2_);
-  positions.push_back(2.0*joint1_pos1_-joint1_pos2_);
-  positions.push_back(2.0*joint2_pos1_-joint2_pos2_);
-  positions.push_back(2.0*joint3_pos1_-joint3_pos2_);
-  positions.push_back(2.0*joint4_pos1_-joint4_pos2_);
-//  positions.push_back(grip_joint_pos_);
-//  positions.push_back(grip_joint_pos_); // grip_joint_sub is same as grip_joint
-
-  ros::Duration duration;
-  duration.sec = duration_sec;
-
-  trajectory_msgs::JointTrajectory joint_trajectory;
-
-  trajectory_msgs::JointTrajectoryPoint arm_joint_point;
-
-  joint_trajectory.points.push_back(arm_joint_point);
-
-  joint_trajectory.joint_names = names;
-  joint_trajectory.points[0].positions = positions;
-  joint_trajectory.points[0].time_from_start = duration;
-
-  publisher.publish(joint_trajectory);
-}
 
 
 int SIGVerseTb3OpenManipulatorGraspingTeleopKey::calcTrajectoryDuration(const double val, const double current_val)
@@ -268,20 +184,10 @@ void SIGVerseTb3OpenManipulatorGraspingTeleopKey::showHelp()
   puts("a: Strafe Left");
   puts("---------------------------");
   puts("u: Upward");
-  //puts("j: Rotate Arm - Horizontal");
   puts("m: Downward");
   puts("---------------------------");
   puts("1: Turn Left");
-  //puts("2: Joint1 Left");
   puts("3: Turn Right");
-  /*puts("4: Joint2 Down");
-  puts("5: Joint3 Up");
-  puts("6: Joint3 Down");
-  puts("7: Joint4 Up");
-  puts("8: Joint4 Down");
-  puts("---------------------------");
-  puts("o: Hand Open");
-  puts("c: Hand Close");*/
   puts("---------------------------");
   puts("h: Show help");
 }
@@ -383,90 +289,7 @@ void SIGVerseTb3OpenManipulatorGraspingTeleopKey::keyLoop(int argc, char** argv)
           moveBase(pub_base_twist, 0.0, +ANGULAR_VEL);
           break;
         }
-        case KEY_U:
-        {
-          ROS_DEBUG("Rotate Arm - Upward");
-          moveArm(pub_joint_traj, JOINT2_NAME, 0.0, joint2_pos1_);
-          moveArm(pub_joint_traj, JOINT3_NAME, 0.0, joint3_pos1_);
-          moveArm(pub_joint_traj, JOINT4_NAME, 0.0, joint4_pos1_);
-          break;
-        }
-        case KEY_J:
-        {
-          ROS_DEBUG("Rotate Arm - Horizontal");
-          moveArm(pub_joint_traj, JOINT2_NAME, +1.57, joint2_pos1_);
-          moveArm(pub_joint_traj, JOINT3_NAME, -1.57, joint3_pos1_);
-          moveArm(pub_joint_traj, JOINT4_NAME, +0.26, joint4_pos1_);
-          break;
-        }
-        case KEY_M:
-        {
-          ROS_DEBUG("Rotate Arm - Downward");
-          moveArm(pub_joint_traj, JOINT2_NAME, +1.75, joint2_pos1_);
-          moveArm(pub_joint_traj, JOINT3_NAME, -1.57, joint3_pos1_);
-          moveArm(pub_joint_traj, JOINT4_NAME, +0.26, joint4_pos1_);
-          break;
-        }
-        case KEY_1:
-        {
-          ROS_DEBUG("Joint1 Right");
-          moveArm(pub_joint_traj, JOINT1_NAME, JOINT_MIN, joint1_pos1_);
-          break;
-        }
-        case KEY_2:
-        {
-          ROS_DEBUG("Joint1 Left");
-          moveArm(pub_joint_traj, JOINT1_NAME, JOINT_MAX, joint1_pos1_);
-          break;
-        }
-        case KEY_3:
-        {
-          ROS_DEBUG("Joint2 Up");
-          moveArm(pub_joint_traj, JOINT2_NAME, JOINT_MIN, joint2_pos1_);
-          break;
-        }
-        case KEY_4:
-        {
-          ROS_DEBUG("Joint2 Down");
-          moveArm(pub_joint_traj, JOINT2_NAME, JOINT_MAX, joint2_pos1_);
-          break;
-        }
-        case KEY_5:
-        {
-          ROS_DEBUG("Joint3 Up");
-          moveArm(pub_joint_traj, JOINT3_NAME, JOINT_MIN, joint3_pos1_);
-          break;
-        }
-        case KEY_6:
-        {
-          ROS_DEBUG("Joint3 Down");
-          moveArm(pub_joint_traj, JOINT3_NAME, JOINT_MAX, joint3_pos1_);
-          break;
-        }
-        case KEY_7:
-        {
-          ROS_DEBUG("Joint4 Up");
-          moveArm(pub_joint_traj, JOINT4_NAME, JOINT_MIN, joint4_pos1_);
-          break;
-        }
-        case KEY_8:
-        {
-          ROS_DEBUG("Joint4 Down");
-          moveArm(pub_joint_traj, JOINT4_NAME, JOINT_MAX, joint4_pos1_);
-          break;
-        }
-        case KEY_O:
-        {
-          ROS_DEBUG("Hand Open");
-          moveHand(pub_joint_traj, GRIP_MIN, grip_joint_pos1_);
-          break;
-        }
-        case KEY_C:
-        {
-          ROS_DEBUG("Hand Close");
-          moveHand(pub_joint_traj, GRIP_MAX, grip_joint_pos1_);
-          break;
-        }
+
         case KEY_H:
         {
           ROS_DEBUG("Show Help");
